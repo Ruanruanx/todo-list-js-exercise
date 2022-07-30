ff1db2f1180c028fe68ece2847e4f2e1cbc537ef
Ruanruanx <zxin7@uwo.ca>
2022-07-30 16:38:09 +0000

    use Method

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 // Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
-const taskDescriptions = [];
+
 // Create a new task by adding to the arrays
 // A new task will be created as incomplete
 function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+    logTaskState: function(){
+      console.log(`${this.title} has ${this.complete?"":"not"} been completed`);
+    },
+    markCompleted: function(){
+      this.complete = true;
+    }
   };
   return task;
 }
+// function newTask(title, description) {
+//   taskTitles.push(title);
+//   taskDescriptions.push(description);
+//   taskComplete.push(false);
+// }
+// function logTaskState(task) {
+//   console.log( `${task.title} has  ${task.complete ? " " : "not"} been complated` )}
 
-function logTaskState(task) {
-  console.log( `${task.title} has  ${task.complete ? " " : "not"} been complated` )}
-
-function completeTask(task){
-  task.complete=true;
-}
+// function completeTask(task){
+//   task.complete=true;
+// }
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
-const tasks = [task1, task2];
-
-console.log(tasks);
+let task = [task1, task2];
+task1.logTaskState();
+task1.markCompleted();
+task1.logTaskState();
 
 // // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
 // function completeTask(taskIndex) {
@@ -38,11 +47,11 @@ console.log(tasks);
 //   console.log(`${title} has${complete ? " " : " not "}been completed`);
 // }
 
-// // DRIVER CODE BELOW
+// // // DRIVER CODE BELOW
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+// newTask("Clean Cat Litter"); // task 0
+// newTask("Do Laundry"); // task 1
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
+// logTaskState(0); // Clean Cat Litter has not been completed
+// completeTask(0);
+// logTaskState(0); // Clean Cat Litter has been completed
